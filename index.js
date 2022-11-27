6af4fc3d05396b0ecc533100a717c4aed57bbdb6
Caren-Koroeny <carensiya5@gmail.com>
2022-11-27 20:04:15 +0300

    added files

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-$(function() {
-    $('a[href*=#]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    });
-  });
+new fullpage('#fullpage', {
+	autoScrolling: true,
+	scrollBar: true,
+})
